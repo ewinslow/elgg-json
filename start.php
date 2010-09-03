30278377c38ba04c2e089b77ff68309e81fd2d4e
Evan Winslow <evan.b.winslow@gmail.com>
2010-09-03 03:41:02 +0000

    forgot to update include path to be vendors rather than lib

--- start.php
+++ start.php
@@ -3,15 +3,15 @@
  * Defines json_encode/json_decode functions using the Zend library
  * in case we have a version of php installed which doesn't support them
  */
-require_once 'lib/Zend/Json.php';
+require_once dirname(__FILE__).'/vendors/Zend/Json.php';
 
-if (!is_callable('json_encode')) {
+if (!function_exists('json_encode')) {
 	function json_encode($value) {
 		return Zend_Json::encode($value);
 	}
 }
 
-if (!is_callable('json_decode')) {
+if (!function_exists('json_decode')) {
 	function json_decode($value, $type) {
 		return Zend_Json::decode($value, $type);
 	}
